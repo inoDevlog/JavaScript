bfa71217d765b9586958de579b2bf1351001c4ea
inoDevlog <ino0319@gmail.com>
2018-11-21 00:20:54 +0900

    no message

--- INSIDE_JavaScript/hello_javascript.js
+++ INSIDE_JavaScript/hello_javascript.js
@@ -4,3 +4,11 @@ var foo = {
 };
 console.log(foo.toString());
 console.dir(foo);
+
+var foo = new Array(3);
+console.log(foo);
+console.log(foo.length);
+
+var bar = new Array(1, 2, 3);
+console.log(bar);
+console.log(bar.length);
