3167cc04a666241a915eee6db9262ce49ccb5532
inoDevlog <ino0319@gmail.com>
2018-12-06 02:15:34 +0900

    no message

--- Inside_JS/ch04.js
+++ Inside_JS/ch04.js
@@ -13,3 +13,24 @@ console.log(bar());
 var obj = {};
 obj.baz = function () { return 200; }
 console.log(obj.baz());
+
+var foo = function(func) {
+    func();
+};
+
+foo(function(){
+    console.log('Function can be used as the argument/');
+});
+
+var foo = function() {
+    return function () {
+        console.log('this function is the return value.')
+    };
+};
+var bar = foo();
+bar();
+
+function add(x, y) {
+    return x + y;
+}
+console.dir(add);
